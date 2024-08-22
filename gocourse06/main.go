6e92a249db00c9cd24af9180f55998055ae59822
Oleksandr Redko <oleksandr.red+github@gmail.com>
2024-08-22 18:08:12 +0300

    concurrent wg

--- gocourse06/main.go
+++ gocourse06/main.go
@@ -3,45 +3,55 @@ package main
 import (
 	"fmt"
 	"math/rand/v2"
+	"sync"
 	"time"
 )
 
-func task1(done chan<- struct{}) {
+func task1() {
 	s := time.Duration((rand.N(300))+100) * time.Millisecond
 	fmt.Println("Task 1 sleeping for", s)
 	time.Sleep(s)
 	fmt.Println("Task 1")
-	done <- struct{}{}
 }
 
-func task2(ch chan<- struct{}) {
+func task2() {
 	s := time.Duration((rand.N(300))+100) * time.Millisecond
 	fmt.Println("Task 2 sleeping for", s)
 	time.Sleep(s)
 	fmt.Println("Task 2")
-	ch <- struct{}{}
 }
 
-func task3(done chan<- struct{}) {
+func task3() {
 	s := time.Duration((rand.N(300))+100) * time.Millisecond
 	fmt.Println("Task 3 sleeping for", s)
 	time.Sleep(s)
 	fmt.Println("Task 3")
-	done <- struct{}{}
 }
 
 func main() {
 	now := time.Now()
 
-	done := make(chan struct{})
+	var wg sync.WaitGroup
 
-	go task1(done)
-	go task2(done)
-	go task3(done)
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		task1()
+	}()
 
-	<-done
-	<-done
-	<-done
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		task2()
+	}()
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		task3()
+	}()
+
+	wg.Wait()
 
 	elapsed := time.Since(now)
 	fmt.Println("Elapsed:", elapsed)
