6f97e0cf8a51038b332cacf059b3928356fe27a0
Oleksandr Redko <oleksandr.red+github@gmail.com>
2024-08-22 18:06:06 +0300

    concurrent channel

--- gocourse06/main.go
+++ gocourse06/main.go
@@ -6,33 +6,42 @@ import (
 	"time"
 )
 
-func task1() {
+func task1(done chan<- struct{}) {
 	s := time.Duration((rand.N(300))+100) * time.Millisecond
 	fmt.Println("Task 1 sleeping for", s)
 	time.Sleep(s)
 	fmt.Println("Task 1")
+	done <- struct{}{}
 }
 
-func task2() {
+func task2(ch chan<- struct{}) {
 	s := time.Duration((rand.N(300))+100) * time.Millisecond
 	fmt.Println("Task 2 sleeping for", s)
 	time.Sleep(s)
 	fmt.Println("Task 2")
+	ch <- struct{}{}
 }
 
-func task3() {
+func task3(done chan<- struct{}) {
 	s := time.Duration((rand.N(300))+100) * time.Millisecond
 	fmt.Println("Task 3 sleeping for", s)
 	time.Sleep(s)
 	fmt.Println("Task 3")
+	done <- struct{}{}
 }
 
 func main() {
 	now := time.Now()
 
-	task1()
-	task2()
-	task3()
+	done := make(chan struct{})
+
+	go task1(done)
+	go task2(done)
+	go task3(done)
+
+	<-done
+	<-done
+	<-done
 
 	elapsed := time.Since(now)
 	fmt.Println("Elapsed:", elapsed)
