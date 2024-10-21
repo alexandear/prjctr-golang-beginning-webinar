b0e72064357258796e77d28c48f9dd121fc36581
Oleksandr Redko <oleksandr.red+github@gmail.com>
2024-10-21 17:46:21 +0300

    gocourse23: simplify BytesToString

--- gocourse23/cmd/unsafe/main.go
+++ gocourse23/cmd/unsafe/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"reflect"
 	"unsafe"
 )
 
@@ -55,15 +54,5 @@ func main() {
 
 // BytesToString converts a slice of bytes to a string without additional memory copying.
 func BytesToString(b []byte) string {
-	// Getting SliceHeader, which describes the slice
-	var bh = (*reflect.SliceHeader)(unsafe.Pointer(&b))
-
-	// Creating StringHeader, which describes the string
-	var sh = reflect.StringHeader{
-		Data: bh.Data, // using the same data address
-		Len:  bh.Len,  // the length of the slice determines the length of the string
-	}
-
-	// Converting StringHeader back to a string without copying data
-	return *(*string)(unsafe.Pointer(&sh))
+	return unsafe.String(unsafe.SliceData(b), len(b))
 }
